a6e92cd3c685f20a2d22f23a04846420c2c91382
wisam hakim <zexperto@hotmail.com>
2018-07-20 14:57:21 -0400

    add force actions field

--- app/code/local/Zeo/Recaptcha/Helper/Data.php
+++ app/code/local/Zeo/Recaptcha/Helper/Data.php
@@ -1,64 +1,98 @@
 <?php
+
 class Zeo_Recaptcha_Helper_Data extends Mage_Core_Helper_Abstract
 {
-	const XML_PATH_CFC_PUBLIC_KEY  = 'zeo_recaptcha_setting/general/public_key';
-	const XML_PATH_CFC_PRIVATE_KEY = 'zeo_recaptcha_setting/general/private_key';
-	const XML_PATH_CFC_THEME       = 'zeo_recaptcha_setting/general/theme';
-	const XML_PATH_CFC_LANG        = 'zeo_recaptcha_setting/general/lang';
-	
-	 public static function IsActive(){
-		return !(boolean)Mage::getStoreConfig('advanced/modules_disable_output/Zeo_Recaptcha');
-	}
-	public static function getPublicKey(){
-		$siteKey = Mage::getStoreConfig(self::XML_PATH_CFC_PUBLIC_KEY);
-		return $siteKey;
-	}
-	public static function getPrivateKey(){
-		$siteKey = Mage::getStoreConfig(self::XML_PATH_CFC_PRIVATE_KEY);
-		return $siteKey;
-	}
-	public static function getTheme(){
-		$siteKey = Mage::getStoreConfig(self::XML_PATH_CFC_THEME);
-		return $siteKey;
-	}
-	public static function getLang(){
-		$siteKey = Mage::getStoreConfig(self::XML_PATH_CFC_LANG);
-		return $siteKey;
-	}
-	
-	public  static function addReCaptchaBlock(){
-		if(Mage::helper('recaptcha')->IsActive()){
-			$siteKey = Mage::helper('recaptcha')->getPublicKey();
-			//get reCaptcha theme name
-			$theme = Mage::helper('recaptcha')->getTheme();
-			if (strlen($theme) == 0 || !in_array($theme, array('dark', 'light'))) {
-				$theme = 'light';
-			}
-			
-			//get reCaptcha lang name
-			$lang = Mage::helper('recaptcha')->getLang();
-			if (strlen($lang) == 0) {
-				$lang = 'en';
-			}
-			
-			$text = '<script type="text/javascript">';
-			$text .= 'function renderReCaptcha() {';
-			$text .= 'grecaptcha.render("g-recaptcha", {';
-			$text .= 'sitekey: "'.$siteKey.'",';
-			$text .= 'theme: "'.$theme.'",';
-			$text .= 'lang: "'.$lang.'",';
-			$text .= '});';
-			$text .= '}';
-			$text .= '</script>';
-			$text .= '<div id="g-recaptcha"></div>';
-			$text .= '<input name="zeo_grecaptcha" type="hidden"  value="'.Mage::helper('core/url')->getCurrentUrl().'"/>';
-			return $text;
-		}
-			
-		
-			
-	}
-	
-	//  echo Mage::helper('recaptcha')->addReCaptchaBlock();
+    const XML_PATH_CFC_PUBLIC_KEY = 'zeo_recaptcha_setting/general/public_key';
+    const XML_PATH_CFC_PRIVATE_KEY = 'zeo_recaptcha_setting/general/private_key';
+    const XML_PATH_CFC_THEME = 'zeo_recaptcha_setting/general/theme';
+    const XML_PATH_CFC_LANG = 'zeo_recaptcha_setting/general/lang';
+    const XML_PATH_CFC_FORCE_ACTIONS = 'zeo_recaptcha_setting/general/force_actions';
+
+    public static function IsActive()
+    {
+        return !(boolean)Mage::getStoreConfig('advanced/modules_disable_output/Zeo_Recaptcha');
+    }
+
+    public static function getPublicKey()
+    {
+        $siteKey = Mage::getStoreConfig(self::XML_PATH_CFC_PUBLIC_KEY);
+        return $siteKey;
+    }
+
+    public static function getPrivateKey()
+    {
+        $siteKey = Mage::getStoreConfig(self::XML_PATH_CFC_PRIVATE_KEY);
+        return $siteKey;
+    }
+
+    public static function getTheme()
+    {
+        $siteKey = Mage::getStoreConfig(self::XML_PATH_CFC_THEME);
+        return $siteKey;
+    }
+
+    public static function getLang()
+    {
+        $siteKey = Mage::getStoreConfig(self::XML_PATH_CFC_LANG);
+        return $siteKey;
+    }
+
+    public static function getForceActions()
+    {
+        $lines = explode("\n", Mage::getStoreConfig(self::XML_PATH_CFC_FORCE_ACTIONS));
+        $actions = [];
+        foreach ($lines as $line) {
+            $ac = explode("|", $line);
+            $actions[] = $ac[0];
+        }
+        return $actions;
+    }
+    public static function getForceActionsData()
+    {
+        $lines = explode("\n", Mage::getStoreConfig(self::XML_PATH_CFC_FORCE_ACTIONS));
+        $actions = [];
+        foreach ($lines as $line) {
+            $ac = explode("|", $line);
+            $actions[$ac[0]] = ["session_type"=>$ac[1], "variable"=>$ac[2]];
+        }
+        return $actions;
+    }
+
+
+
+    public static function addReCaptchaBlock()
+    {
+        if (Mage::helper('recaptcha')->IsActive()) {
+            $siteKey = Mage::helper('recaptcha')->getPublicKey();
+            //get reCaptcha theme name
+            $theme = Mage::helper('recaptcha')->getTheme();
+            if (strlen($theme) == 0 || !in_array($theme, array('dark', 'light'))) {
+                $theme = 'light';
+            }
+
+            //get reCaptcha lang name
+            $lang = Mage::helper('recaptcha')->getLang();
+            if (strlen($lang) == 0) {
+                $lang = 'en';
+            }
+
+            $text = '<script type="text/javascript">';
+            $text .= 'function renderReCaptcha() {';
+            $text .= 'grecaptcha.render("g-recaptcha", {';
+            $text .= 'sitekey: "' . $siteKey . '",';
+            $text .= 'theme: "' . $theme . '",';
+            $text .= 'lang: "' . $lang . '",';
+            $text .= '});';
+            $text .= '}';
+            $text .= '</script>';
+            $text .= '<div id="g-recaptcha"></div>';
+            $text .= '<input name="zeo_grecaptcha" type="hidden"  value="' . Mage::helper('core/url')->getCurrentUrl() . '"/>';
+            return $text;
+        }
+
+
+    }
+
+    //  echo Mage::helper('recaptcha')->addReCaptchaBlock();
 }
 	 
--- app/code/local/Zeo/Recaptcha/Model/Observer.php
+++ app/code/local/Zeo/Recaptcha/Model/Observer.php
@@ -1,77 +1,98 @@
 <?php
-class Zeo_Recaptcha_Model_Observer {
-	public function prepareLayoutBefore(Varien_Event_Observer $observer) {
-		$_block = $observer->getBlock ();
-		//get reCaptcha lang name
-		$lang = Mage::helper('recaptcha')->getLang();
-		if (strlen($lang) == 0) {
-			$lang = 'en';
-		}
-			
-		$_block->setText ( '<script src="//www.google.com/recaptcha/api.js?onload=renderReCaptcha&render=explicit&hl='.$lang.'"></script>' );
-		/*
-		 * $block = $observer->getEvent()->getBlock();
-		 *
-		 * if ("head" == $block->getNameInLayout()) {
-		 * Mage::log(get_class($block));
-		 * $block->AddJs('','https://www.google.com/recaptcha/api.js?onload=renderReCaptcha&render=explicit');
-		 * }
-		 */
-		return $this;
-	}
-	public function checkCaptcha($observer) {
-		$action_name = $observer->getEvent ()->getControllerAction ()->getFullActionName ();
-		$controller = $observer->getEvent ()->getControllerAction ();
-		
-		if (true) {
-			$post = Mage::app ()->getRequest ()->getPost ();
-			if ($post) {
-				if (isset ( $post ["zeo_grecaptcha"] )) {
-					if (! isset ( $post ['g-recaptcha-response'] ) || ! $this->isCaptchaValid ( $post ['g-recaptcha-response'] )) {
-						$message = Mage::helper('core')->__ ( "The reCAPTCHA wasn't entered correctly. Go back and try it again." );
-						Mage::getSingleton ( 'core/session' )->addError ( $message );
-							$Url = $post ["zeo_grecaptcha"];
-							Mage::app()->getResponse()->setRedirect($Url)->sendResponse(); 
-							exit;
-					}
-				}
-			}
-		}
-	}
-	/**
-	 * Check if captcha is valid
-	 * 
-	 * @param string $captchaResponse        	
-	 * @return boolean
-	 */
-	private function isCaptchaValid($captchaResponse) {
-		$result = false;
-		
-		$params = array (
-				'secret' => Mage::helper ( 'recaptcha' )->getPrivateKey (),
-				'response' => $captchaResponse 
-		);
-		
-		$ch = curl_init ( 'https://www.google.com/recaptcha/api/siteverify' );
-		curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, 1 );
-		curl_setopt ( $ch, CURLOPT_HEADER, 0 );
-		curl_setopt ( $ch, CURLOPT_POST, 1 );
-		curl_setopt ( $ch, CURLOPT_POSTFIELDS, http_build_query ( $params ) );
-		curl_setopt ( $ch, CURLOPT_VERBOSE, 1 );
-		curl_setopt ( $ch, CURLOPT_SSL_VERIFYPEER, false );
-		curl_setopt ( $ch, CURLOPT_SSL_VERIFYHOST, false );
-		curl_setopt ( $ch, CURLOPT_TIMEOUT, 30 );
-		$requestResult = trim ( curl_exec ( $ch ) );
-		curl_close ( $ch );
-		
-		if (is_array ( json_decode ( $requestResult, true ) )) {
-			$response = json_decode ( $requestResult, true );
-			
-			if (isset ( $response ['success'] ) && $response ['success'] === true) {
-				$result = true;
-			}
-		}
-		
-		return $result;
-	}
+
+class Zeo_Recaptcha_Model_Observer
+{
+    public function prepareLayoutBefore(Varien_Event_Observer $observer)
+    {
+        $_block = $observer->getBlock();
+        //get reCaptcha lang name
+        $lang = Mage::helper('recaptcha')->getLang();
+        if (strlen($lang) == 0) {
+            $lang = 'en';
+        }
+
+        $_block->setText('<script src="//www.google.com/recaptcha/api.js?onload=renderReCaptcha&render=explicit&hl=' . $lang . '"></script>');
+        /*
+         * $block = $observer->getEvent()->getBlock();
+         *
+         * if ("head" == $block->getNameInLayout()) {
+         * Mage::log(get_class($block));
+         * $block->AddJs('','https://www.google.com/recaptcha/api.js?onload=renderReCaptcha&render=explicit');
+         * }
+         */
+        return $this;
+    }
+
+    public function checkCaptcha($observer)
+    {
+        $action_name = $observer->getEvent()->getControllerAction()->getFullActionName();
+        $controller = $observer->getEvent()->getControllerAction();
+        $force_actions = Mage::helper('recaptcha')->getForceActions ();// ["customer_account_createpost"];
+
+        $force_actions_data = Mage::helper('recaptcha')->getForceActionsData();
+      /*  foreach ($force_actions as $item) {
+            $force_actions_data[$item] = ["session_type"=>"customer/session", "variable"=>customer_form_data];
+        }
+*/
+    
+        if (true) {
+            $post = Mage::app()->getRequest()->getPost();
+            if ($post) {
+                $check_reCaptcha = isset ($post ["zeo_grecaptcha"]) || in_array($action_name, $force_actions);
+
+                if ($check_reCaptcha) {
+                    if (!isset ($post ['g-recaptcha-response']) || !$this->isCaptchaValid($post ['g-recaptcha-response'])) {
+                        $message = Mage::helper('core')->__("The reCAPTCHA wasn't entered correctly. Go back and try it again.");
+                        Mage::getSingleton('core/session')->addError($message);
+
+                        if(in_array($action_name, $force_actions)) {
+                            Mage::getSingleton($force_actions_data[$action_name]["session_type"])
+                                ->setData($force_actions_data[$action_name]["variable"],$post);
+                        }
+                        $Url = $post ["zeo_grecaptcha"];
+                        Mage::app()->getResponse()->setRedirect($Url)->sendResponse();
+                        exit;
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Check if captcha is valid
+     *
+     * @param string $captchaResponse
+     * @return boolean
+     */
+    private function isCaptchaValid($captchaResponse)
+    {
+        $result = false;
+
+        $params = array(
+            'secret' => Mage::helper('recaptcha')->getPrivateKey(),
+            'response' => $captchaResponse
+        );
+
+        $ch = curl_init('https://www.google.com/recaptcha/api/siteverify');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));
+        curl_setopt($ch, CURLOPT_VERBOSE, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+        $requestResult = trim(curl_exec($ch));
+        curl_close($ch);
+
+        if (is_array(json_decode($requestResult, true))) {
+            $response = json_decode($requestResult, true);
+
+            if (isset ($response ['success']) && $response ['success'] === true) {
+                $result = true;
+            }
+        }
+
+        return $result;
+    }
 }
